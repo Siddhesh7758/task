c3a3c85ffc174c6b2674852b27b4bec8cc0a29a3
Rushizzz <rushikeshshinde760@gmail.com>
2023-03-31 23:24:29 +0530

    rushi commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import { FormControlLabel, Switch } from "@mui/material";
 function CountryList() {
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
@@ -60,37 +60,83 @@ function CountryList() {
 
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search countries"
-        value={searchTerm}
-        onChange={handleSearch}
-      />
-      <div className="ml-3 m-3">
-        <button
-          className="bg-gray-400 p-2 rounded"
-          onClick={handleShowSelected}
-        >
-          Show selected countries
-        </button>
-        <button className="bg-gray-400 p-2 rounded ml-2" onClick={handleShowAll}>
-          Reset
-        </button>
-      </div>
-      {filteredCountries.map((country) => (
-        <div key={country.name.common}>
-          <label>
-            <input
-              type="checkbox"
-              name={country.name.common}
-              onChange={handleSelect}
-              checked={selectedCountries.includes(country.name.common)}
+    
+    <div className="w-screen h-screen flex items-center justify-center">
+
+      <style>
+        {`
+          ::-webkit-scrollbar {
+            width: 0.5rem;
+            border-radius: 9999px;
+          }
+
+          ::-webkit-scrollbar-track {
+            background: transparent;
+          }
+
+          ::-webkit-scrollbar-thumb {
+            background: #888;
+            border-radius: 9999px;
+          }
+
+          ::-webkit-scrollbar-thumb:hover {
+            background: #555;
+          }
+        `}
+      </style>
+
+      <div className="absolute rounded-[14px] bg-white shadow-[9px_32px_35px_rgba(0,_0,_0,_0.05)] flex flex-col p-5 items-start justify-start border-[1px] border-solid border-gainsboro-100">
+        <div className="flex flex-col items-start justify-start">
+          <input
+            className="border-b border-custom-gray font-campton text-base bg-[transparent] self-stretch relative h-8 shrink-0"
+            type="text"
+            placeholder="Search"
+            onChange={handleSearch}
+          />
+          <div className="w-[454px] flex flex-row py-[23px] px-0 box-border items-center justify-between">
+            <FormControlLabel
+              className="relative"
+              label="Show selected only"
+              control={<Switch color="primary" size="medium" />}
+              onClick={handleShowSelected}
             />
-            {country.name.common}
-          </label>
+            <button className="cursor-pointer [border:none] p-0 bg-[transparent] relative w-[60px] h-[22px] shrink-0" onClick={handleShowAll}>
+              <div className="absolute top-[calc(50%_-_11px)] left-[0%] text-base tracking-[-0.5px] leading-[22px] font-medium font-campton text-black text-left">
+                Clear all
+              </div>
+            </button>
+          </div>
+          {/* below srcollbar with countries code */}
+          <div className="self-stretch relative h-[247px] shrink-0 overflow-y-auto">
+              {filteredCountries.map((country) => (
+                <div key={country.name.common}>
+                  <label className="h-10 flex items-center">
+                    <input
+                      type="checkbox"
+                      name={country.name.common}
+                      onChange={handleSelect}
+                      checked={selectedCountries.includes(country.name.common)}
+                      className="mr-2"
+                    />
+                    {country.name.common}
+                  </label>
+                </div>
+              ))}
+            </div>
+          {/* below line code */}
+          <div className="relative rounded-xl bg-whitesmoke w-[437.12px] h-px shrink-0" />
+
+          <div className="self-stretch flex flex-col pt-5 px-0 pb-0 items-end justify-center">
+            <button className="cursor-pointer [border:none] p-0 bg-green rounded-[50px] flex flex-row items-start justify-start">
+              <div className="rounded-[27.5px] flex flex-row py-2 px-[25px] items-center justify-center border-[1px] border-solid border-green">
+                <div className="relative text-base tracking-[-0.5px] font-campton-book text-white text-center">
+                  Save
+                </div>
+              </div>
+            </button>
+          </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/App.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderColor: {
+        'custom-gray': '#ECECEC',
+      },
+      colors: {
+        gainsboro: { "100": "#e1e3e6", "200": "#e1e1e1" },
+        white: "#fff",
+        green: "#60d09b",
+        whitesmoke: "#ececec",
+        darkslategray: { "100": "#343434", "200": "rgba(52, 52, 52, 0.51)" },
+        gray: "rgba(0, 0, 0, 0.02)",
+        lavender: {
+          "100": "rgba(237, 226, 254, 0)",
+          "200": "rgba(210, 223, 252, 0)",
+        },
+        moccasin: "rgba(255, 234, 182, 0)",
+        lightcyan: "rgba(208, 240, 253, 0)",
+        mistyrose: {
+          "100": "rgba(254, 226, 213, 0)",
+          "200": "rgba(255, 220, 229, 0)",
+        },
+        royalblue: "#457df1",
+        black: "#232323",
+      },
+      fontFamily: { "campton-book": "Campton-Book", campton: "Campton" },
+      borderRadius: { "2xs": "11px" },
+    },
+    fontSize: { base: "16px" },
   },
-  plugins: [],
-}
-
+  corePlugins: { preflight: false },
+};
