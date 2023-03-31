4129ff9c7f5cf8d6e25ee263947d6c8aa0b59d0b
siddhesh munfan <munfansiddhesh7758@gmail.com>
2023-03-31 20:58:28 +0530

    half done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+function CountryList() {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        const sortedData = data.sort((a, b) =>
+          a.name.common.localeCompare(b.name.common)
+        );
+        setCountries(sortedData);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col">
+      <input className="w-40 border-black"
+        type="text"
+        placeholder="Search countries"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      {filteredCountries.map((country) => (
+
+        <div key={country.name.common} class="flex items-center">
+          <input
+            id="link-checkbox"
+            type="checkbox"
+            value=""
+            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            for="link-checkbox"
+            class="ml-2 text-sm font-medium text-gray-600 "
+          >
+            <h2>{country.name.common}</h2>
+          </label>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default CountryList;
